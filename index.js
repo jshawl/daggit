4b2aef65aa21f2a0e398f4cf9a02aba1984f0110
Jesse Shawl <jesse@jshawl.com>
2016-03-22 11:43:16 -0400

    make some progress

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var util = require('util')
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) {
-  console.log(stdout)
-}
-exec("ls -laFG", puts);
+var express = require("express");
+var app = express();
+app.set("view engine", "hbs")
+var exec = require('child_process').execSync;
+var Convert = require('ansi-to-html');
+var convert = new Convert();
+
+
+app.get("/", function(req, res){
+  var log = exec("git log --all --oneline --graph --decorate --color=always",{encoding: 'utf8'})
+  var html = convert.toHtml(log);
+  html = html.replace(/>([a-z0-9]+)</g,function(sha){
+    var sha = sha.replace(/(<|>)/g,"")
+    return "><a href='/"+sha+"'>" +sha+ "</a><"
+  })
+  var diff = exec("git show HEAD --color=always",{encoding: 'utf8'})
+  diff = convert.toHtml(diff);
+  res.render("index", {log: html, diff: diff})
+})
+app.listen(3000)
