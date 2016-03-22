2f7920601ebc45d3014c1d80fc08d687e2f1e355
Jesse Shawl <jesse@jshawl.com>
2016-03-22 11:45:22 -0400

    render sha view

--- index.js
+++ index.js
@@ -18,4 +18,17 @@ app.get("/", function(req, res){
   diff = convert.toHtml(diff);
   res.render("index", {log: html, diff: diff})
 })
+
+app.get("/:sha", function(req, res){
+  var log = exec("git log --all --oneline --graph --decorate --color=always",{encoding: 'utf8'})
+  var html = convert.toHtml(log);
+  html = html.replace(/>([a-z0-9]+)</g,function(sha){
+    var sha = sha.replace(/(<|>)/g,"")
+    return "><a href='/"+sha+"'>" +sha+ "</a><"
+  })
+  var diff = exec("git show "+req.params.sha+" --color=always",{encoding: 'utf8'})
+  diff = convert.toHtml(diff);
+  res.render("index", {log: html, diff: diff})
+})
+
 app.listen(3000)
