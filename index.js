0bbfd1355c56b4fc2e490f52652cd060a84eb508
Jesse Shawl <jesse@jshawl.com>
2016-03-22 15:33:50 -0400

    refactor

--- index.js
+++ index.js
@@ -10,30 +10,34 @@ var exec = require('child_process').execSync;
 var Convert = require('ansi-to-html');
 var convert = new Convert();
 
-
 app.get("/", function(req, res){
-  var log = exec("git log --all --oneline --graph --decorate --color=always",{encoding: 'utf8'})
-  var html = convert.toHtml(log);
-  html = html.replace(/>([a-z0-9]+)</g,function(sha){
-    var sha = sha.replace(/(<|>)/g,"")
-    return "><a href='/"+sha+"'>" +sha+ "</a><"
-  })
-  var diff = exec("git show HEAD --color=always",{encoding: 'utf8'})
-  diff = convert.toHtml(diff);
-  res.render("index", {log: html, diff: diff})
+  res.render("index", {log: log(), diff: diff()})
 })
 
 app.get("/:sha", function(req, res){
-  var log = exec("git log --all --oneline --graph --decorate --color=always",{encoding: 'utf8'})
-  var html = convert.toHtml(log);
-  html = html.replace(/>([a-z0-9]+)</g,function(sha){
-    var sha = sha.replace(/(<|>)/g,"")
-    return "><a href='/"+sha+"'>" +sha+ "</a><"
-  })
-  var diff = exec("git show "+req.params.sha+" --color=always",{encoding: 'utf8'})
-  diff = convert.toHtml(diff);
-  res.render("index", {log: html, diff: diff})
+  res.render("index", {log: log(req.params.sha), diff: diff(req.params.sha)})
 })
+
+function log(reqsha){
+  var l = exec("git log -n 30 --all --oneline --graph --decorate --color=always",{encoding: 'utf8'})
+  var html = convert.toHtml(l);
+  return html.replace(/>([a-z0-9]+)</g,function(sha){
+    var sha = sha.replace(/(<|>)/g,"")
+    var str = "><a "
+    if(reqsha == sha){
+      str += "class='active' "
+    }
+    str += "href='/"+sha+"'>" +sha+ "</a><"
+    return str
+  })
+}
+
+function diff(sha){
+  var sha = sha || "HEAD"
+  var d = exec("git show "+sha+" --color=always",{encoding: 'utf8'});
+  d = d.replace(/</g,"&lt;")
+  return convert.toHtml(d);
+}
 
 app.listen(3000, function(){
   console.log("listening on http://localhost:3000/")
